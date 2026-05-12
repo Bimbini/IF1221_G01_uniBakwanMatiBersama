ef1e7c7e1d3583e834c9810ff052f37960ee18cb
araended <arrsyakira@gmail.com>
2026-05-12 19:39:50 +0700

    refractor: discard_pile to last_played

--- src/discardpile.pl
+++ src/discardpile.pl
@@ -2,7 +2,7 @@
 :- include('facts.pl').
 
 :- dynamic(deck/1).
-:- dynamic(discard_pile/1).
+:- dynamic(last_played/1).
 
 % Validasi jika kartu tidak termasuk kategori kartu angka
 notAngka(kartu(hitam, _)).
@@ -17,8 +17,8 @@ discardPile :-
 
     retractall(deck(_)),
     asserta(deck(Rest)),
-    retractall(discard_pile(_)),
-    asserta(discard_pile([FirstCard])),
+    retractall(last_played(_)),
+    asserta(last_played(start, FirstCard)),
 
     FirstCard = kartu(X, Y),
     write('Kartu discard top: '), write(X), write('-'), write(Y), write('.'), nl, nl.
@@ -27,8 +27,6 @@ discardPile :-
 % jika kartu yang diambil bukan kartu angka
 takeDiscardPile(List, FirstCard, Rest):-
     random_pick(List, X, Rest1),
-    notAngka(X), !,
-    takeDiscardPile(Rest1, FirstCard, Rest).
-
-takeDiscardPile(List, FirstCard, Rest) :-
-    random_pick(List, FirstCard, Rest).
+    (   \+ notAngka(X) -> FirstCard = X,
+                          Rest = Rest1;
+        takeDiscardPile(Rest1, FirstCard, Rest)).
