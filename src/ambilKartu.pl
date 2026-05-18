9d2c545a1a048ff2cefe52e1018fd872faeb10fe
eronessa <danessasherin@gmail.com>
2026-05-18 18:24:09 +0700

    Refactor ambilKartu.pl for clarity and structure

--- src/ambilKartu.pl
+++ src/ambilKartu.pl
@@ -1,5 +1,5 @@
-:- include ('random7cards.pl').
-:- include ('giliran.pl').
+:- include('random7cards.pl').
+:- include('giliran.pl').
 
 % mulai setelah currentPlayer
 ambilKartu :-
@@ -19,6 +19,6 @@ ambilNKartu(N, Player) :-
     takeNrandom(1, DeckBefore, [Card], DeckAfter),
     assertz(hand(Player,Card)),
     updateDeck(DeckAfter),
-    format("~w mendapatkan kartu: ~w~-n", [Player, Kartu]),
+    format("~w mendapatkan kartu: ~w~-n", [Player, Card]),
     N1 is N - 1,
     ambilNKartu(N1, Player).
