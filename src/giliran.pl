fedf01eb633272b2ccd6165da5130225f2ce2308
eronessa <danessasherin@gmail.com>
2026-05-19 14:19:08 +0700

    fix: giliran based on rotation (ada clockwise counterclockwise)

--- src/giliran.pl
+++ src/giliran.pl
@@ -8,15 +8,28 @@ add_tail([H|T], X, [H|L]) :- add_tail(T, X, L).
 /* when called keluarin dulu sekarang giliran siapa 
 baru list update the next person di paling depan list */
 currentPlayer :-
-    giliran([H|T]), 
+    giliran([H | _]),
     format('Giliran ~w.~n~n', [H]),
+    (arah(clockwise) -> rotate_kiri ; rotate_kanan).
+
+rotate_kiri :-
+    giliran([H | T]),
     add_tail(T, H, NewGiliran),
     updateList(NewGiliran).
+
+rotate_kanan :-
+    giliran(G),
+    init_last(G, Front, Last),
+    NewGiliran = [Last | Front],
+    updateList(NewGiliran).
+
+% Helper rotate_kanan karna append GABOLEH
+init_last([X], [], X).
+init_last([H|T], [H|Front], Last) :- init_last(T, Front, Last).
 
 updateList(NewGiliran) :-
     retract(giliran(_)),
     assertz(giliran(NewGiliran)).
-
 
 setPlayers(Players) :-
     retractall(giliran(_)),
