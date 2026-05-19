2a0c021f022c1d69d8c10c8f8a133bedaa52a5e6
Samantha <samantha.michelle2007@gmail.com>
2026-05-19 14:15:11 +0700

    fix: change rule

--- src/tangkap.pl
+++ src/tangkap.pl
@@ -5,9 +5,9 @@
 tangkap(Player) :- player_hand(Player, Cards),
                 hitung_kartu(Cards, 1),
                 \+sudah_uni(Player),!,
-                format('~w tertangkap tidak menyerukan UNI. ~n ~w mendapatkan 2 kartu penalti. ~n', [Player, Player]),
+                format('~w tertangkap tidak menyerukan UNI. ~n~w mendapatkan 2 kartu penalti. ~n', [Player, Player]),
                 ambilNKartuPenalti(Player,2),
-                pindah_giliran.
+                currentPlayer.
 
 tangkap(_) :- giliran([Penangkap|_]),
                     format('Perintah tidak valid! Anda mendapatkan 1 kartu penalti.'), nl,
