69ec73332666b9821c7843bff6fb7a06065c989b
eronessa <danessasherin@gmail.com>
2026-05-19 15:42:52 +0700

    fix: update list dulu baru sebutin giliran

--- src/giliran.pl
+++ src/giliran.pl
@@ -9,35 +9,24 @@ add_tail([H|T], X, [H|L]) :- add_tail(T, X, L).
 /* when called keluarin dulu sekarang giliran siapa 
 baru list update the next person di paling depan list */
 currentPlayer :-
-    giliran([H | _]),
-    format('Giliran ~w.~n~n', [H]),
     arah(clockwise), !,
     rotate_kiri.
 
 currentPlayer :-
-    giliran([H | _]),
-    format('Giliran ~w.~n~n', [H]),
+    arah(counter_clockwsie),
     rotate_kanan.
 
 rotate_kiri :-
     giliran([H | T]),
     add_tail(T, H, NewGiliran),
     updateList(NewGiliran).
+    NewGiliran = [Next|_],
+    format('Giliran ~w.~n~n', [Next]).
+
 
 rotate_kanan :-
     giliran(G),
     init_last(G, Front, Last),
     NewGiliran = [Last | Front],
-    updateList(NewGiliran).
-
-% Helper rotate_kanan karna append GABOLEH
-init_last([X], [], X).
-init_last([H|T], [H|Front], Last) :- init_last(T, Front, Last).
-
-updateList(NewGiliran) :-
-    retract(giliran(_)),
-    assertz(giliran(NewGiliran)).
-
-setPlayers(Players) :-
-    retractall(giliran(_)),
-    assertz(giliran(Players)).
+    updateList(NewGiliran),
+    format('Giliran ~w.~n~n', [Next]).
