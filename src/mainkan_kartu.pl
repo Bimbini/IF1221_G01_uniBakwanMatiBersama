d27dcca8b5d95b420f4ea4fe338589e88e318aaa
grevaurely <aurelynecia@gmail.com>
2026-05-19 11:12:13 +0700

    Update mainkan_kartu.pl

--- src/mainkan_kartu.pl
+++ src/mainkan_kartu.pl
@@ -1,6 +1,9 @@
 :- dynamic(last_played/2). % revisi 
 :- dynamic(player_hand/2).
+:- dynamic(urutan_pemain/1).
+urutan_pemain([pemain1, pemain2]).
 :- dynamic(current_player/1).
+
 :- dynamic(discard_top/1).
 :- dynamic(current_color/1).
 
@@ -14,22 +17,25 @@ mainkanKartu(Urutan):-
     player_hand(PemainSaatIni,Handlist),                % ambil kartu yg sekarang dipegang
     (ambilKartu(Urutan,Handlist,kartu(W,J)) ->           % ambil kartu yg sesuai Input
         discard_top(kartu(_ ,J_kartu_di_meja)),
-        current_color(W_aktif),
-        ( (W == W_aktif) ; (J == J_kartu_di_meja) ; (W == hitam) ->
-            buang_kartu(PemainSaatIni, kartu(W,J)) 
-        ;
+        current_color(W_kartu_di_meja),
+        (can_throw(kartu(W, J), kartu(W_kartu_di_meja, J_kartu_di_meja), Handlist) -> 
+        % dari rules
+            buang_kartu(PemainSaatIni, kartu(W, J)),
+            pindah_giliran
+        ; 
             write('Gagal, kartu tidak cocok denga warna atau jenis seperti yang ada di meja'), nl, fail
-        ) 
+        )
     ;
         write('Gagal, kartu tersebut tidak kamu miliki!'), nl, fail
     ).    
 
-pindah_giliran :-
-    retract(urutan_pemain([Current | Sisa])), % Ambil pemain sekarang
-    append(Sisa, [Current], UrutanBaru),      % Pindahkan dia ke antrian belakang
-    assertz(urutan_pemain(UrutanBaru)),       % Simpan urutan baru
 
-    UrutanBaru = [Next | _],                  % Pemain berikutnya adalah yang paling depan
+pindah_giliran :-
+    retract(urutan_pemain([Current | Sisa])), 
+    append(Sisa, [Current], UrutanBaru),      
+    assertz(urutan_pemain(UrutanBaru)),       
+
+    UrutanBaru = [Next | _],                  
     retract(current_player(_)),
     assertz(current_player(Next)),
     format('Sekarang giliran: ~w~n', [Next]).
@@ -46,9 +52,9 @@ buang_kartu(Pemain, kartu(W,J)) :-
     assertz(last_played(Pemain, kartu(W, J))),
 
     % kalo wildcard dipake
-    (W == hitam -> 
+(W == hitam -> 
         write('Kartu Hitam! Masukkan warna baru (merah/biru/hijau/kuning): '), 
-        read(WarnaBaru), % Mengambil input dari keyboard pemain
+        read(WarnaBaru), 
         retract(current_color(_)), 
         assertz(current_color(WarnaBaru)),
         format('Warna sekarang berubah menjadi: ~w~n', [WarnaBaru])
@@ -61,5 +67,10 @@ buang_kartu(Pemain, kartu(W,J)) :-
     ; 
         write('giliran ke pemain berikutnya...'), nl
     ).
+
+    % to do list : 
+    % 1. efek draw_two
+    % 2. efek wild_draw_four
+    % 3. efek skip
 
 
