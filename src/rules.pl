2a9d5e0ebb7e9135ea4c6535d59ae53d17e81497
grevaurely <aurelynecia@gmail.com>
2026-05-29 18:02:19 +0700

    update wild card

--- src/rules.pl
+++ src/rules.pl
@@ -10,26 +10,24 @@ is_card(kartu(hitam,J)) :- wild_card(J).
 
 /* aturan permainan */
 /* can_throw(cardInHand, cardOnTable, playerHandList)*/
+
+% wild card, draw_two, wild_draw_four gaboleh ditumpuk dua kali
+
+can_throw(kartu(hitam, wild), kartu(_,wild), _) :- !, fail.
+
+can_throw(kartu(_,draw_two), kartu(_,draw_two), _) :- !, fail.
+
+can_throw(kartu(hitam, wild_draw_four), kartu(_, wild_draw_four), _) :- !, fail.
+
+
 % warna sama
 can_throw(kartu(W,_), kartu(W,_), _).
 
 % jenis/angka sama
 can_throw(kartu(_,J), kartu(_,J), _).
 
-% wild card bisa dipake kpn aja
+% wild bisa dipake kapan aja
 can_throw(kartu(hitam,wild), _, _).
 
-% wild card four gaboleh ditumpuk dua kali
-can_throw(kartu(_,draw_two), kartu(_,draw_two), _) :- !, fail.
-
-can_throw(kartu(hitam, wild_draw_four), kartu(_, wild_draw_four), _) :- !, fail.
-
+% wild_draw_four bisa dipake kapan aja
 can_throw(kartu(hitam,wild_draw_four), _, _).
-
-
-
-
-
-
-
-
